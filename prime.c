ed667ad686b5a795a291f9c2ec012f6ce25b3417
Prince <princejangra146@gmail.com>
2023-05-09 15:35:52 +0530

    primne c no. program

---
+++ prime.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+int prime (int n)
+{
+    int i;
+    for(i=2;i<n; i++)
+    {
+        if(n%i != 0)
+        return 0;
+        else
+        return 1;
+
+
+    }
+}
+int main(){
+
+int num,res=0;
+
+printf(" enter the number=");
+scanf("%d" ,&num);
+res=prime(num);
+if(res==0)
+printf("\n%d is a prime number", num );
+else
+printf( "\n%d is not a prime number ", num);
+return 0;
+}`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
